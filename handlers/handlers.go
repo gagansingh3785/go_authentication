c32965c29ee6d1fbed64a9c73ec4c3fcc5483cd8
gagan <gaganpreet.singh@gojek.com>
2023-02-06 22:33:05 +0530

    [Gagan] Removed templates from home path

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,22 +8,12 @@ import (
 	"github.com/gagansingh3785/go_authentication/constants"
 	"github.com/gagansingh3785/go_authentication/requests"
 	"github.com/gagansingh3785/go_authentication/responses"
-	"html/template"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	//This is how templates are executed in golang
-	path, err := constants.GetTemplatePath()
-	if err != nil {
-		fmt.Fprintf(w, "Response:%s Status:%s", constants.InternalServerError, http.StatusInternalServerError)
-	}
-	t, err := template.New("Home").ParseFiles(path + "/" + constants.HomeHTMLFile)
-	if err != nil {
-		fmt.Fprintf(w, "Response:%s Status:%s", constants.InternalServerError, http.StatusInternalServerError)
-	}
-	err = t.Execute(w, "")
-	fmt.Println(err)
+	fmt.Fprintf(w, "This is the home page")
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
