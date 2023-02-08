07b6ac18ece872b54562374285a1c199221a25ef
gagan <gaganpreet.singh@gojek.com>
2023-02-08 20:29:36 +0530

    [Gagan] Changed send mail handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,11 +1,8 @@
 package handlers
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/gagansingh3785/go_authentication/config"
-	"github.com/gagansingh3785/go_authentication/constants"
 	"github.com/gagansingh3785/go_authentication/requests"
 	"github.com/gagansingh3785/go_authentication/responses"
 	"net/http"
@@ -45,34 +42,34 @@ func SendMail(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Domain processing and making API call to MailGrid
-	url := config.GlobalConfig.SendGrid.APIHost + config.GlobalConfig.SendGrid.APIEndpoint
-	contentType := constants.CONTENT_TYPE
-	receiverName := "gagan"
-	receiverEmail := "9592951585g@gmail.com"
+	//url := config.GlobalConfig.SendGrid.APIHost + config.GlobalConfig.SendGrid.APIEndpoint
+	//contentType := constants.CONTENT_TYPE
+	//receiverName := "gagan"
+	//receiverEmail := "9592951585g@gmail.com"
 
-	GridMailRequest := requests.GetSendGridRequestBody(sendMailRequest.Name,
-		receiverName,
-		sendMailRequest.Email,
-		receiverEmail,
-		config.GlobalConfig.SendGrid.APIKey,
-		constants.CONTENT_TYPE,
-		sendMailRequest.Message,
-	)
-	req, err := json.Marshal(GridMailRequest)
-	bodyReader := bytes.NewReader(req)
-	fmt.Println("url:" + url)
-	response, err := http.Post(url, contentType, bodyReader)
-	if err != nil {
-		fmt.Println("here2")
-		fmt.Fprintf(w, err.Error())
-		return
-	}
-	if response.StatusCode != 200 || response.StatusCode != 201 {
-		fmt.Println("here3")
-		fmt.Println(response.Status)
-		fmt.Fprintf(w, response.Status)
-		return
-	}
+	//GridMailRequest := requests.GetSendGridRequestBody(sendMailRequest.Name,
+	//	receiverName,
+	//	sendMailRequest.Email,
+	//	receiverEmail,
+	//	config.GlobalConfig.SendGrid.APIKey,
+	//	constants.CONTENT_TYPE,
+	//	sendMailRequest.Message,
+	//)
+	//req, err := json.Marshal(GridMailRequest)
+	//bodyReader := bytes.NewReader(req)
+	//fmt.Println("url:" + url)
+	//response, err := http.Post(url, contentType, bodyReader)
+	//if err != nil {
+	//	fmt.Println("here2")
+	//	fmt.Fprintf(w, err.Error())
+	//	return
+	//}
+	//if response.StatusCode != 200 || response.StatusCode != 201 {
+	//	fmt.Println("here3")
+	//	fmt.Println(response.Status)
+	//	fmt.Fprintf(w, response.Status)
+	//	return
+	//}
 	// Response processing
 	handlerResp := responses.SendMailResponse{
 		Message: "Success! We will get back to you soon :)",
